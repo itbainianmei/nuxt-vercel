2d2c2c9d65b8cbf17e431236acbbaaf9b480f6fc
张英美 <1935686874@qq.com>
2021-06-15 11:38:06 +0800

    test3

---
+++ nuxt.config.js
@@ -0,0 +1,5 @@
+export default {
+    ssr:true //服务端渲染
+    // ssr:false 客户端渲染
+    // ssr : 'static' 生成静态网站
+}
---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <nuxt-link to="/home">to home</nuxt-link>
+      <h1>about page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
